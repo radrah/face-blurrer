afcf95b80110de02eb1bcb2123b46fb01917bb8c
radrah <raduwan.rahman@hotmail.com>
2024-04-22 18:17:39 +1200

    WIP detecting face and making a box around it

--- pixelated-face.js
+++ pixelated-face.js
@@ -9,4 +9,42 @@ ctx.strokeStyle = '#ffc600'
 ctx.lineWidth = 2
 
 const faceCanvas = document.querySelector('.face ')
-const faceCtx = canvas.getContext('2d')const faceDectector = new FaceDetector();// Write a function to populate users videoasync function populateVideo(){    const stream = await navigator.mediaDevices.getUserMedia({        video: { width: 1280, height: 720 },});    video.srcObject = stream    await video.play()    canvas.width = video.videoWidth    canvas.height = video.videoHeight    faceCanvas.width = video.videoWidth    faceCanvas.height = video.videoHeight}async function detect(){    const faces = await faceDectector.detect(video)    // console.log(faces.length);    faces.forEach(drawFace)    faces.forEach(censor)    requestAnimationFrame(detect)}function drawFace(face) {    const { width, height, top, left } = face.boundingBox    ctx.clearRect(0, 0, canvas.width, canvas.height)    ctx.strokeRect(left, top, width, height)}function censor({boundingBox: face}){    console.log(face);}populateVideo().then(detect)
+const faceCtx = canvas.getContext('2d')
+const faceDectector = new FaceDetector();
+
+// Write a function to populate users video
+
+async function populateVideo(){
+    const stream = await navigator.mediaDevices.getUserMedia({
+        video: { width: 1280, height: 720 },
+
+});
+
+    video.srcObject = stream
+    await video.play()
+
+    canvas.width = video.videoWidth
+    canvas.height = video.videoHeight
+    faceCanvas.width = video.videoWidth
+    faceCanvas.height = video.videoHeight
+}
+
+async function detect(){
+    const faces = await faceDectector.detect(video)
+    // console.log(faces.length);
+    faces.forEach(drawFace)
+    faces.forEach(censor)
+    requestAnimationFrame(detect)
+}
+
+function drawFace(face) {
+    const { width, height, top, left } = face.boundingBox
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.strokeRect(left, top, width, height)
+}
+
+function censor({boundingBox: face}){
+    console.log(face);
+}
+
+populateVideo().then(detect)
